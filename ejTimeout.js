5e0355b2e3ebf394f9db3f1ec24cf4f2ab76e927
Alex-GarciaM <garciserpent@gmail.com>
2023-02-10 10:23:26 +0100

    Enunciado ej propuesto

---
+++ ejTimeout.js
@@ -0,0 +1,8 @@
+/*
+Crear un objeto alumno con nombre, apellido y NIA.
+Crear una función que acepte cualquier número de parámetros (rest)
+Imprimir (por consola) un alumno (toda su info) cada 3 segundos.
+Calcular el número de parámetros que se han introducido sin el çuso 
+de un contador.
+Imprimir el MAX(NIA)
+*/
